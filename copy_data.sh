64c14a7c2e5c9cbb79bccf1f93be6482b6b7aad8
Elephant34 <51174423+Elephant34@users.noreply.github.com>
2025-03-24 11:16:16 +0000

    Create copy_data.sh

---
+++ copy_data.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+cp -uv /home/pi/Documents/masters-Electronics/logs/* /home/pi/Documents/masters-Data/logs
+cp -uv /home/pi/Documents/masters-Electronics/data/* /home/pi/Documents/masters-Data/data
